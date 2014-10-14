ea6c36619111b87454a6b1fc17396fd2d6ea8ae0
se314064 <se314064@a8c17012-ad29-4552-9e37-3a93cacc5ebe>
2014-10-14 15:08:03 +0000

    fixed exercise_1_4.cpp
    git-svn-id: https://svn.cs.nuim.ie/2014_2015/CS264/main/se314064@955 a8c17012-ad29-4552-9e37-3a93cacc5ebe

--- Lab2/exercise_1_4.cpp
+++ Lab2/exercise_1_4.cpp
@@ -2,13 +2,11 @@
 #include<cstring>
 using namespace std;
 
-int length;
 void mystringlen(char str[]){
 	cout << strlen(str) << endl;
 }
 void mystringcat(char dest[], char src[], int dest_size){
-	int size_of_src = strlen(src);	
-	int total_size = length + size_of_src;
+	int total_size = strlen(dest) + strlen(src);
 	if(total_size < dest_size){
 		cout << strcat(dest, src) << endl;
 	}else
@@ -17,15 +15,12 @@ void mystringcat(char dest[], char src[], int dest_size){
 
 int main(){
 	char str[] = "keith";
-	cout << "Enter a size for dest: ";
-	cin >> length; 
-	char *dest = new char[length];
+	char dest[15]; 
 	char src[] = "keith";
-	cout << "Enter a word: ";
 	cin >> dest;
 	strcpy(src, str);
 
 	mystringlen(str);
-	mystringcat(dest, src, length);
+	mystringcat(dest, src, 15);
 
 }
