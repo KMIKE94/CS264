c42362a0dd981d5c53545696c4acd96cbb466cb0
se314064 <se314064@a8c17012-ad29-4552-9e37-3a93cacc5ebe>
2014-10-16 12:25:26 +0000

    added complete exercise_1_3
    git-svn-id: https://svn.cs.nuim.ie/2014_2015/CS264/main/se314064@1168 a8c17012-ad29-4552-9e37-3a93cacc5ebe

--- Lab3/exercise_1_3.cpp
+++ Lab3/exercise_1_3.cpp
@@ -7,17 +7,21 @@ struct person{
 };
 
 int main(){
-	int length;
+	int num_people;
 	cout << "How many people? ";
-	cin >> length;
-	person *people = new person[length];
-	
-	for(int i = -1; i < length; ++i){
+	cin >> num_people;
+
+	person *people = new person[num_people];
+
+	for(int i = 0; i < num_people; i++){
 		cout << "Enter name : ";
 		cin >> people[i].name;
-		cout << "\n";
 		cout << "Enter age : ";
-		cin >> people[i].age;
-		cout << "\n";
+		cin >> people[i].age;	
 	}
+
+	for(int i = num_people-1; i >= 0; --i){
+		cout << (people+i)->name << " " << (people+i)->age << endl;
+	}
+
 }
