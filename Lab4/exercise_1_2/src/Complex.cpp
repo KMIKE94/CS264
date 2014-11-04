488cb8ceab7129cf3375664539a679ebdbc511ea
se314064 <se314064@a8c17012-ad29-4552-9e37-3a93cacc5ebe>
2014-11-04 16:48:24 +0000

    fixed multiplication of complex and real numbers
    git-svn-id: https://svn.cs.nuim.ie/2014_2015/CS264/main/se314064@1796 a8c17012-ad29-4552-9e37-3a93cacc5ebe

--- Lab4/exercise_1_2/src/Complex.cpp
+++ Lab4/exercise_1_2/src/Complex.cpp
@@ -4,53 +4,57 @@ using namespace std;
 
 double result_real;
 double result_imag;
-Complex::Complex(){
+Complex::Complex() {
 	real = 0;
 	imag = 0;
 }
 
-Complex::Complex(double a, double b){
+Complex::Complex(double a, double b) {
 	real = a;
 	imag = b;
 }
 
-void Complex::print(){
+void Complex::print() {
 	cout << "(" << this->real << ", " << this->imag << ")";
 }
 
-double Complex::getReal(){
+double Complex::getReal() {
 	return real;
 }
 
-void Complex::setReal(double a){
+void Complex::setReal(double a) {
 	real = a;
 }
 
-double Complex::getImag(){
+double Complex::getImag() {
 	return imag;
 }
 
-void Complex::setImag(double b){
+void Complex::setImag(double b) {
 	imag = b;
 }
 
-Complex Complex::Add(const Complex &x){
+Complex Complex::Add(const Complex &x) {
 	result_real = this->real + x.real;
 	result_imag = this->imag + x.imag;
 	Complex temp(result_real, result_imag);
 	return temp;
 }
 
-Complex Complex::Subtract(const Complex &x){
+Complex Complex::Subtract(const Complex &x) {
 	result_real = this->real - x.real;
 	result_imag = this->imag - x.imag;
 	Complex temp(result_real, result_imag);
 	return temp;
 }
 
-Complex Complex::Multiply(const Complex &x){
+Complex Complex::Multiply(const Complex &x) {
 	result_real = this->real * x.real;
-	result_imag = this->imag * x.imag;
-	Complex temp(result_real, result_imag);
-	return temp;
+	int result_2 = this->real * x.imag;
+	result_imag = this->imag * x.real;
+	int result_4 = this->imag * x.imag;
+	result_real = -result_4 + result_real;
+	result_imag = (result_2 + result_imag);
+	Complex result(result_real, result_imag);
+	return result;
 }
